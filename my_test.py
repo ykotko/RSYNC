b99373757e10f6607175e8d70b71b279cbcf864c
Egor <ykotko@mirantis.com>
2014-01-13 19:19:23 +0200

    tmp

--- my_test.py
+++ my_test.py
@@ -9,21 +9,11 @@ content = a.readlines()
 for l in content:
     if l.find('data') != -1:
         data = l.split()[1]
-    
-#content = a.readlines()
-for l in content:
     if l.find('user') != -1:
         user = l.split()[1]
-
-#content = a.readlines()
-for l in content:
-    if l.find('pass') != -1:
+    if l.find('passwd') != -1:
         passwd = l.split()[1]
        
-
-content = a.read()
-data = content.find('/etc/hosts')
-
 #cmdargs = (sys.argv)
 #ip = sys.argv[1]
 #path = sys.argv[2]
@@ -39,24 +29,18 @@ else:
     f = open('path_file', 'w+')
     f.write(path)
 try:
-#    rc = "rsync -rzv {0}:{1}:{2}".format(ip, data, path)
-#    subprocess.call( rc.split(' ') )
-    subprocess.call(['rsync' ,'-rzv' ,data,user+'@'+ip+':'+path])
+    rc = "rsync -rzv {0} {1}@{2}:{3}".format(data,user,ip,path)
+    subprocess.call( rc.split(' ') )
+#    subprocess.call(['rsync' ,'-rzv' ,data,user+'@'+ip+':'+path])
+#    subprocess.call('rsync' ,'-rzv' ,data,'{user}@{ip}:{path}'.format(user = user, ip = ip, path = path))
 except Exception as e:
     print 'Exception {var}'.format(var = e)
 
-ip = str(ip)
-user = str(user)
-#passd = str(passd)
-print ip
-print user
-#print passwd
 
 cl = Client(host = ip, username = user, password = passwd)
-#cl = Client(host = '172.18.166.197', username = 'root', password = 'r00tme')	
 #cl.exec_command("a=(`ps axuf | grep -v grep | grep supervisor | awk '{print $2}'`); if [[ -n \"$a\" ]]; then kill $a; else echo 'No supervisord process'; fi")
 s = cl.exec_command("ps axuf | grep -v grep | grep supervisor") # hole string with pid 
 rc = s.split()
-cl.exec_command("kill {var}".format(var = rc[1])) # will give a <pid> of process supervisord
+cl.exec_command("kill {var}".format(var = rc[1]))
 
 
